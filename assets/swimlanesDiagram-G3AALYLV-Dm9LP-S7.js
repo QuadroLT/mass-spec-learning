d9cd971120059cfdc860628a3e528bf21e24644f
QuadroLT <v.tamosiunas@gmail.com>
2026-08-28 01:41:43 +0300

    Updates

--- assets/swimlanesDiagram-G3AALYLV-Dm9LP-S7.js
+++
@@ -1,8 +0,0 @@
-import"./src-CG1yvd_Q.js";import"./chunk-WYO6CB5R-N3W6-L0B.js";import{n as e}from"./chunk-Y2CYZVJY-DsF7k-Jl.js";import"./chunk-ICXQ74PX-DAIdnVdd.js";import"./chunk-C7G6YPKG-BwspDKiI.js";import"./chunk-OGEWGWER-v-HgFDmo.js";import"./chunk-HOUHSVGY-BH88pziW.js";import"./chunk-Q4XR5HBZ-DJM7Sf5O.js";import"./chunk-ZGVPDNZ5-CBAtmpt3.js";import"./chunk-7BUUIJ7U-Bb538aSH.js";import"./index-sRhyazbV.js";import"./chunk-32BRIVSS-CQDjmzbI.js";import"./chunk-XXDRQBXY-Bjq2-PTQ.js";import"./chunk-VR4S4FIN-DC8hDXN1.js";import{r as t,t as n}from"./chunk-PUDLZKDR-DGTvi_A-.js";var r=n({defaultLayout:`swimlane`,styles:e(e=>`${t(e)}
-  .swimlane.cluster rect {
-    stroke: ${e.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,`getStyles`)});export{r as diagram};
